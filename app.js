0c93c8a4d6161c8ff558a869fa3485af1075e79d
anton000 <anthony@milkfishstudio.com>
2013-12-31 03:37:08 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var ProductImage = Backbone.Model.extend({ 
+	
+	readImage: function(file) {
+	var reader = new FileReader();
+    
+    
+  // closure to capture the file information.
+  reader.onload = (function(theFile,model) {
+		console.log('fname : ' + this.cid);
+    return function(e) {
+    	//set model
+			model.set({filename: theFile.name, filedata: e.target.result});
+			model.save();
+			};
+		})(file,this);
+		
+		// Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+		//return this;
+    } 
+});
